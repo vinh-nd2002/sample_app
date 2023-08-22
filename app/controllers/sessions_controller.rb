8f6ad818058efe0f39db9459ff13b777318ed597
vinh-nd2002 <nguduyvinh2k2@gmail.com>
2023-08-22 15:34:23 +0700

    Chapter 9: Advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,19 +2,46 @@ class SessionsController < ApplicationController
   def new; end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user&.authenticate(params[:session][:password])
-      reset_session
-      log_in user
-      redirect_to user
+    user = find_user_by_email
+    if user_authenticated?(user)
+      handle_successful_login(user)
     else
-      flash.now[:danger] = t "login.danger_flash"
-      render :new, status: :unprocessable_entity
+      handle_failed_login
     end
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url, status: :see_other
+  end
+
+  private
+
+  def find_user_by_email
+    @user = User.find_by email: params.dig(:session, :email)&.downcase
+  end
+
+  def user_authenticated? user
+    user&.authenticate(params[:session][:password])
+  end
+
+  def handle_successful_login user
+    reset_session
+    remember_or_forget user
+    log_in(user)
+    redirect_to(user)
+  end
+
+  def remember_or_forget user
+    if params[:session][:remember_me] == "1"
+      remember(user)
+    else
+      forget(user)
+    end
+  end
+
+  def handle_failed_login
+    flash.now[:danger] = t("login.danger_flash")
+    render :new, status: :unprocessable_entity
   end
 end
